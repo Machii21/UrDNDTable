d9764df78aea2dbf4e2992e37a4365f1149c27a0
Machii21 <pazajoshua@gmail.com>
2021-07-19 09:17:49 -0700

    I think it works o-o

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { StyleSheet, Text, View, Button } from 'react-native';
 
 import Home from "./screens/Home";
 import Player from "./screens/Player";
+import Navigation from "./config/Navigation";
 
 export default function App() {
   return (
